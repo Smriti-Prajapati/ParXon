b2bf8af7d1e61a47f2bc2549e0e079dd891e73df
Smriti-Prajapati <smritiprajapati15@gmail.com>
2025-02-18 01:05:47 +0530

    fixed bugs

--- app/src/main/java/com/example/parxondemo1/GameActivity.java
+++ app/src/main/java/com/example/parxondemo1/GameActivity.java
@@ -14,8 +14,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.lang.reflect.Array;
-
 public class GameActivity extends AppCompatActivity {
 
     ListView listView;
@@ -26,53 +24,46 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_game);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        String[] tstory = getResources().getStringArray(R.array.title1_story);
-        final String[] dstory = getResources().getStringArray(R.array.details_story1_links);
+        // Load game names and links from strings.xml
+        String[] gameTitles = getResources().getStringArray(R.array.title1_story);
+        final String[] gameLinks = getResources().getStringArray(R.array.details_story1_links);
 
-
-
-        listView=findViewById(R.id.list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.row,R.id.rowtxt,tstory);
+        listView = findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, gameTitles);
         listView.setAdapter(adapter);
 
-
-
+        // Handle game selection
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                String t = dstory[position];
-                Intent intent =new Intent(GameActivity.this,GameActivityDetails.class);
-                intent.putExtra("story",t);
+                String selectedGameLink = gameLinks[position];
+
+                // Open GameActivityDetails and pass the game link
+                Intent intent = new Intent(GameActivity.this, GameActivityDetails.class);
+                intent.putExtra("game_link", selectedGameLink);
                 startActivity(intent);
             }
         });
-
     }
 
     public void gamegoback(View view) {
-
-        Intent intent = new Intent(GameActivity.this,MainActivity.class);
+        Intent intent = new Intent(GameActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
-
-
-
-
     }
 
     @Override
     public void onBackPressed() {
-
         super.onBackPressed();
-        Intent intent = new Intent(GameActivity.this,MainActivity.class);
+        Intent intent = new Intent(GameActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
-
     }
 }
